6d7b501e8517a941d097a8394bc7f4ed7e4b31ea
SmilingPlatipus <jpbenoit@laposte.net>
2020-02-23 20:53:51 +0100

    Tests unitaires OK Tests instrumentalisés RecyclerView vide / Ajout d'une réunion / Suppression d'une réunion

--- app/src/androidTest/java/com/example/mareu/MainActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mareu/MainActivityInstrumentedTest.java
@@ -20,15 +20,15 @@ import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.swipeDown;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.hasChildCount;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
-import static androidx.test.espresso.matcher.ViewMatchers.withResourceName;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.AllOf.allOf;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.*;
 /**
@@ -59,39 +59,45 @@ public class MainActivityInstrumentedTest
 
 
     @Test
-    public void meetingsList_addMeetingShouldAddOneMeetingToRecyclerView() {
+    public void meetingsList_addMeetingShouldAddOneMeetingToRecyclerView_ThenRemoveIt() {
         onView(ViewMatchers.withId(R.id.buttonAddMeeting)).perform(click());
         onView(ViewMatchers.withId(R.id.detail_meeting_name)).perform(click(),typeText("MeetingTest"),pressImeActionButton());
 
         onView(ViewMatchers.withId(R.id.button_date)).perform(click());
         onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2020, 3, 30));
         onView(withText("OK"))
-                .perform(click())
                 .inRoot(isDialog())
                 .check(matches(isDisplayed()))
                 .perform(click());
 
-        onView(allOf(ViewMatchers.isDisplayed(), ViewMatchers.withId(R.id.button_start_time))).perform(click());
+        onView(ViewMatchers.withId(R.id.button_start_time)).perform(click());
         onView(withClassName(Matchers.equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(12, 0));
         onView(withText("OK"))
-                .perform(click())
                 .inRoot(isDialog())
                 .check(matches(isDisplayed()))
                 .perform(click());
 
-        onView(allOf(ViewMatchers.isDisplayed(), ViewMatchers.withId(R.id.button_end_time))).perform(click());
+        onView(ViewMatchers.withId(R.id.button_end_time)).perform(click());
         onView(withClassName(Matchers.equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(12,30));
         onView(withText("OK"))
-                .perform(click())
                 .inRoot(isDialog())
                 .check(matches(isDisplayed()))
                 .perform(click());
 
-        onView(ViewMatchers.withId(R.id.detail_meeting_add_email)).perform(click(),typeText("email@lamzone.fr"),pressImeActionButton());
+        onView(ViewMatchers.withId(R.id.main_scrollview)).perform(swipeUp());
+        onView(ViewMatchers.withId(R.id.detail_meeting_add_email)).perform(typeText("email@lamzone.fr"),pressImeActionButton());
+        onView(ViewMatchers.withId(R.id.main_scrollview)).perform(swipeUp());
         onView(ViewMatchers.withId(R.id.button_ok)).perform(click());
 
-        onView(ViewMatchers.withId(R.layout.list_meeting)).check(matches(hasChildCount(1)));
+        onView(ViewMatchers.withId(R.id.recyclerview)).check(matches(hasChildCount(1)));
+        onView(ViewMatchers.withId(R.id.meeting_delete_button)).perform(click());
+        ViewMatchers.withId(R.layout.empty_recyclerview).matches(isDisplayed());
+    }
 
+    @Test
+    public void meetingsList_addMeetingsToRecyclerView_ThenTestFilters(){
 
     }
+
+
 }
